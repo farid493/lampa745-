44cf0392eac34b79face35e7299521ba3c86b15f
farid493 <civasmotors@gmail.com>
2026-09-19 19:05:36 +0400

    Update turkish_test.js

--- turkish_test.js
+++ turkish_test.js
@@ -1,52 +1,19 @@
 (function () {
     'use strict';
 
-    function init() {
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type !== 'complite') return;
-
-            var movie = e.data && e.data.movie;
-            if (!movie) return;
-
-            try {
-                var render = e.object.activity.render();
-
-                if (render.find('.view--turkish').length) return;
-
-                var button = $(
-                    '<div class="full-start__button selector view--turkish">' +
-                        '<span>🇹🇷 TURKISH</span>' +
-                    '</div>'
-                );
-
-                button.on('hover:enter', function () {
-                    var info = [
-                        'Title: ' + (movie.title || 'N/A'),
-                        'Original: ' + (movie.original_title || movie.original_name || 'N/A'),
-                        'IMDb ID: ' + (movie.imdb_id || 'N/A'),
-                        'KP ID: ' + (movie.kinopoisk_id || 'N/A')
-                    ].join('\n');
-
-                    Lampa.Noty.show('--- MOVIE DATA ---\n' + info, { time: 10000 });
-                });
-
-                var torrent = render.find('.view--torrent');
-                if (torrent.length) {
-                    torrent.after(button);
-                } else {
-                    render.find('.full-start__buttons').append(button);
-                }
-            } catch (err) {
-                console.log('Turkish plugin render error:', err);
-            }
-        });
+    if (typeof Lampa === 'undefined') {
+        return;
     }
 
-    if (window.appready) {
-        init();
-    } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') init();
+    Lampa.Noty.show('🇹🇷 TURKISH PLUGIN LOADED', {
+        time: 10000
+    });
+
+    Lampa.Listener.follow('full', function (e) {
+        if (e.type !== 'complite') return;
+
+        Lampa.Noty.show('FULL EVENT WORKS', {
+            time: 5000
         });
-    }
+    });
 })();
