a2b7da335c69d3caeb0eefa1e31d55288560a6e7
farid493 <civasmotors@gmail.com>
2026-09-19 20:33:41 +0400

    Create turkish_test_999.js

---
+++ turkish_test_999.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    if (!window.Lampa) return;
+
+    function start() {
+        Lampa.Noty.show('🔥 TURKISH 999 LOADED', { time: 5000 });
+
+        Lampa.Listener.follow('full', function (e) {
+            Lampa.Noty.show('full: ' + e.type, { time: 4000 });
+        });
+
+        Lampa.Listener.follow('activity', function (e) {
+            if (e.component === 'full') {
+                Lampa.Noty.show('activity/full: ' + e.type, { time: 4000 });
+            }
+        });
+    }
+
+    if (window.appready) {
+        start();
+    } else {
+        if (Lampa.Listener && Lampa.Listener.follow) {
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type === 'ready') start();
+            });
+        } else {
+            start();
+        }
+    }
+})();
