b4cc19169f216d001831d3ef0b7100e7112b3f61
farid493 <civasmotors@gmail.com>
2026-09-19 19:01:38 +0400

    Update turkish_test.js

--- turkish_test.js
+++ turkish_test.js
@@ -1,48 +1,52 @@
 (function () {
     'use strict';
 
-    function start() {
+    function init() {
         Lampa.Listener.follow('full', function (e) {
             if (e.type !== 'complite') return;
 
             var movie = e.data && e.data.movie;
             if (!movie) return;
 
-            var render = e.object.activity.render();
+            try {
+                var render = e.object.activity.render();
 
-            if (render.find('.view--turkish').length) return;
+                if (render.find('.view--turkish').length) return;
 
-            var button = $(
-                '<div class="full-start__button selector view--turkish">' +
-                    '<span>🇹🇷 TURKISH</span>' +
-                '</div>'
-            );
+                var button = $(
+                    '<div class="full-start__button selector view--turkish">' +
+                        '<span>🇹🇷 TURKISH</span>' +
+                    '</div>'
+                );
 
-            button.on('hover:enter', function () {
-                var info = [
-                    'Title: ' + (movie.title || 'N/A'),
-                    'Original: ' + (movie.original_title || movie.original_name || 'N/A'),
-                    'IMDb ID: ' + (movie.imdb_id || 'N/A'),
-                    'KP ID: ' + (movie.kinopoisk_id || 'N/A')
-                ].join('\n');
+                button.on('hover:enter', function () {
+                    var info = [
+                        'Title: ' + (movie.title || 'N/A'),
+                        'Original: ' + (movie.original_title || movie.original_name || 'N/A'),
+                        'IMDb ID: ' + (movie.imdb_id || 'N/A'),
+                        'KP ID: ' + (movie.kinopoisk_id || 'N/A')
+                    ].join('\n');
 
-                Lampa.Noty.show('--- MOVIE DATA ---\n' + info, { time: 10000 });
-            });
+                    Lampa.Noty.show('--- MOVIE DATA ---\n' + info, { time: 10000 });
+                });
 
-            var torrent = render.find('.view--torrent');
-            if (torrent.length) {
-                torrent.after(button);
-            } else {
-                render.find('.full-start__buttons').append(button);
+                var torrent = render.find('.view--torrent');
+                if (torrent.length) {
+                    torrent.after(button);
+                } else {
+                    render.find('.full-start__buttons').append(button);
+                }
+            } catch (err) {
+                console.log('Turkish plugin render error:', err);
             }
         });
     }
 
     if (window.appready) {
-        start();
+        init();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') start();
+            if (e.type === 'ready') init();
         });
     }
 })();
